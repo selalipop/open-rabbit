af83df56469c67f89ffe33c09ec2a9e1d26ce440
Ryota Yoda <ryotayoda@RyotanoMBP.local.meter>
2024-05-19 11:12:24 -0700

    update header ui

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -1,4 +1,4 @@
-import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import {
   createServerClient,
   type CookieOptions,
